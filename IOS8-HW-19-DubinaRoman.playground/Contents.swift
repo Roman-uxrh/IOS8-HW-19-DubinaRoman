630dd1cb69dbbe5fedce5b2878c36b81a4721f64
RRDubina <dubinaroman.dubina@gmail.com>
2023-02-28 14:39:37 +0500

    Change - add print

--- IOS8-HW-19-DubinaRoman.playground/Contents.swift
+++ IOS8-HW-19-DubinaRoman.playground/Contents.swift
@@ -3,13 +3,19 @@ import UIKit
 func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
     guard let url = urlRequest else { return }
+    
     URLSession.shared.dataTask(with: url) { data, response, error in
         if error != nil {
-            
+            print(error?.localizedDescription ?? "")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print(response.statusCode)
             guard let data = data else { return }
-            let dataAsString = String(data: data, encoding: .utf8)
             
+            let dataAsString = String(data: data, encoding: .utf8)
+            print(dataAsString ?? "")
         }
     }.resume()
 }
+
+let url = "https://http.dog/"
+getData(urlRequest: url)
